ba74da4d21052db8784d53422324f5c844d69351
Carlos Soto <Carlos.Soto@users.noreply.github.com>
2022-01-26 14:38:37 -0500

    Commit

---
+++ static/teaching.js
@@ -0,0 +1,32 @@
+export default {
+	intro: ` "I hate math." That's the most common reaction I hear from people when I tell them what I do
+			for a living. This reaction lives, rent-free, in my mind constantly. How is it that so many
+			people dislike an entire subject that I find... well... beautiful? In most cases I have found
+			it all boils down to one thing, how math is taught. In many cases people learn math algorithmically,
+			which can be very tedious, rather than gaining the insight and intuition needed to 
+			understand the reasoning behind algorithms. When writing lesson plans, I put intuition first 
+			and details second. This may seem counterintuitive, but without intuition, I have found, students 
+			never gain the motivation needed to follow through with the mathematics. This is at the core of 
+			my teaching philosphy. For a more in-depth statement, please email me.
+			`,
+	quote: ` <em>The essence of mathematics lies entirely in its freedom.</em> -Georg Cantor`,
+  courses: [
+	{
+      title: `<strong>Instructor</strong>`,
+      dates: `Spring 2021, Fall 2021`,
+      school: '<em>Pennsylvania State University</em>',
+	  location: '<em>State College, PA</em>',
+	  description: `Instructor for STAT 380: Data Science Through Statistical Reasoning and Computation.
+				Resonsible for the entire course except for grading.`,
+    },
+	{
+      title: `<strong>Instructor</strong>`,
+      dates: `Spring 2021, Fall 2021`,
+      school: '<em>Pennsylvania State University</em>',
+	  location: '<em>State College, PA</em>',
+	  description: `Instructor for STAT 380: Data Science Through Statistical Reasoning and Computation.
+				Resonsible for the entire course except for grading.`,
+    },
+
+  ],
+}
