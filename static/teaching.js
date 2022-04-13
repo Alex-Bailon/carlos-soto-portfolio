473bef4fb4263928eef58a79241a1d329d84a80e
Carlos Soto <Carlos.Soto@users.noreply.github.com>
2022-04-13 13:37:43 -0400

    update

--- static/teaching.js
+++ static/teaching.js
@@ -1,7 +1,7 @@
 export default {
 	intro: ` "I hate math." That's the most common reaction I hear from people when I tell them what I do
-			for a living. This reaction lives, rent-free, in my mind constantly. How is it that so many
-			people dislike an entire subject that I find... well... beautiful? In most cases I have found
+			for a living. This reaction lives, rent-free, in my mind. How is it that so many
+			people dislike an entire subject that I find... well... beautiful? In most cases, I have found
 			it all boils down to one thing, how math is taught. In many cases people learn math algorithmically,
 			which can be very tedious, rather than gaining the insight and intuition needed to 
 			understand the reasoning behind algorithms. When writing lesson plans, I put intuition first 
@@ -21,12 +21,29 @@ export default {
     },
 	{
       title: `<strong>Instructor</strong>`,
-      dates: `Spring 2021, Fall 2021`,
-      school: '<em>Pennsylvania State University</em>',
-	  location: '<em>State College, PA</em>',
-	  description: `Instructor for STAT 380: Data Science Through Statistical Reasoning and Computation.
-				Resonsible for the entire course except for grading.`,
+      dates: `Fall 2016 - Spring 2020`,
+      school: '<em>Florida State University</em>',
+	  location: '<em>Tallahassee, FL</em>',
+	  description: `Instructor for STA 2171: Statistics for Biology, responsible for lecturing 
+				and creating all coursework including worksheets and exams.`,
+    },	
+	{
+      title: `<strong>Teaching Assistant</strong>`,
+      dates: `Fall 2015 - Spring 2016`,
+      school: '<em>Florida State University</em>',
+	  location: '<em>Tallahassee, FL</em>',
+	  description: `Teaching Assistant for CGS 2518: Spreadsheets for Business, responsible for assisting 
+				students with assignments as well as grading assignments and exams. `,
     },
+	{
+      title: `<strong>Instructor</strong>`,
+      dates: `Fall 2013 - Spring 2015`,
+      school: '<em>University of Wisconsin–Milwaukee</em>',
+	  location: '<em>Milwaukee, WI</em>',
+	  description: `Instructor for MATH 098, MATH 108, and MATH 105, (College alebgra courses) responsible for 
+	  lecturing and creating all coursework including worksheets and exams.`,
+    },
+
 
   ],
 }
