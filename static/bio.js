dd46d012b96ac583d806b81929f9549458bf3167
Carlos Soto <cjsoto.695@gmail.com>
2026-01-13 08:56:01 -0500

    first person

--- static/bio.js
+++ static/bio.js
@@ -1,25 +1,18 @@
-export default `Carlos is currently an assistant professor in the Department of Mathematics
+export default `I am an assistant professor in the Department of Mathematics
 and Statistics at <a href="https://www.umass.edu/mathematics-statistics/" target="_blank"> University of Massachusetts -- Amherst (UMass Amherst)</a>.
-He was previously a Lindsay Visiting Assistant Research Professor 
-(postdoctoral researcher) in the Department of Statistics at <a href="https://science.psu.edu/stat" target="_blank"> Pennsylvania State University</a>.
-There he worked with Aleksandra (Sesa) Slavkovic and Matthew Reimherr to develop methods at the intersection of differential privacy and statistical shape analysis. 
+Previously, I was the Lindsay Visiting Assistant Research Professor in the Department of Statistics at <a href="https://science.psu.edu/stat" target="_blank"> Pennsylvania 
+State University</a>. I received my PhD and an MS in biostatistics from <a href="https://stat.fsu.edu/" target="_blank">Florida State University</a> and
+an MS in mathematics at <a href="https://uwm.edu/math/" target="_blank">University of Wisconsin -- Milwaukee</a>.
 
 <p>
-In 2020 Carlos received his PhD, and an MS in 2018, in biostatistics from <a href="https://stat.fsu.edu/" target="_blank">Florida State University</a>
- under the supervision of Anuj Srivastava where he investigated the shape and structural properties of chromosomes and proteins. There he was introduced to
- shape analysis which is an area of research somewhere between differential geometry and statistics. </p>
+My research interests include shape analysis, geometric statistics, functional data analysis, differential privacy, and  </p>
+
+<p>
+My previous advisors include (alphabetically) Jay Beder, Matthew Reimherr, Aleksandra (Sesa) Slavkovic, and Anuj Srivastava. </p>
  
-<p>Even further back, in 2015 he received an MS in mathematics at <a href="https://uwm.edu/math/" target="_blank">University of Wisconsin -- Milwaukee</a>
-under the supervision of Jay Beder. His academic career started at <a href="https://ripon.edu/" target="_blank">Ripon College</a> where he received a BA in mathematics in 2011. 
-At the time, and 
-at the time of writing this, Ripon did not offer a statistics degree, so there he did the equivalent of a double major in mathematics and statistics. </p>
+<p> Other: </p>
 
-    
-
-
-<p> Carlos J. Soto grew up in Chicago, the city that never sleeps.  The pandemic has changed his non-academic interests greatly, but in general
-  he is interested in rock climbing, lifting, comedy, gaming, and music (partial to guitar from 1970's to 1990's). He has played guitar, on and off, for nearly 20 years
-  after having learned at the Chicago Old Town School of Folk. He has rock climbed in a few states (Alabama, Georgia, Kentucky, Tennessee, Massachusetts, Illinois, Wisconsin, 
-  Colorado) and hopes to continue to explore crags across the country (5.10 outdoor/5.11 indoor, V5 climber). Even though he'd never admit it, Carlos likes to write poetry.
-  His favorite authors include Kurt Vonnegut, Neil Gaiman, and Manfredo P. Do Carmo. </p>
+<p> I am interested in rock climbing, lifting, gaming, and music. I am an avid guitarist having played for nearly 20 years
+  I have rock climbed in Alabama, Georgia, Kentucky, Tennessee, Massachusetts, Illinois, Wisconsin, and
+  Colorado. (5.10 outdoor/5.11 indoor, V5 climber). </p>
 `
